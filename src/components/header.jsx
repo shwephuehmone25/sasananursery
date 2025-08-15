ca9887e94ce8bbd7e18fa1e41d368fd2538b0a23
shwephuehmone25 <shwephue7889@gmail.com>
2025-08-15 13:16:42 +0630

    feat(header): add slider & image carousel for hero section

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,29 +1,63 @@
 import React from "react";
+import Slider from "react-slick";
 
 export const Header = (props) => {
+  const images = [
+    "img/intro-bg.jpg",
+    "https://sasananurserymonastery.com/wp-content/uploads/2019/09/Slider.jpg",
+    "img/intro-bg-2.jpg"
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    adaptiveHeight: false,
+  };
+
   return (
     <header id="header">
-      <div className="intro">
-        <div className="overlay">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
-                <h2 className="title">
-                  {props.data ? props.data.title : "Loading"}
-                  <span></span>
-                </h2>
-                <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a
-                  href="#features"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Read More
-                </a>
+      <Slider {...settings}>
+        {images.map((img, index) => (
+          <div key={index}>
+            <div
+              className="intro"
+              style={{
+                background: `url(${img}) center center no-repeat`,
+                backgroundSize: "cover",
+                height: "100vh",
+                width: "100vw", 
+                overflow: "hidden", 
+              }}
+            >
+              <div className="overlay">
+                <div className="container">
+                  <div className="row justify-content-center">
+                    <div className="col-md-8 intro-text text-center">
+                      <h2 className="title">
+                        {props.data ? props.data.title : "Loading"}
+                        <span></span>
+                      </h2>
+                      <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                      <a
+                        href="#features"
+                        className="btn btn-custom btn-lg page-scroll"
+                      >
+                        Read More
+                      </a>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        ))}
+      </Slider>
     </header>
   );
 };
